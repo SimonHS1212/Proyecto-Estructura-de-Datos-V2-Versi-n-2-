d1658bceb93719f0ccb5dddb3df1da2989e5c64d
Chajud <167790293+RanHC30@users.noreply.github.com>
2024-06-27 17:48:07 -0600

    Add files via upload

---
+++ Proyecto Estructura de Datos V2 (Versión 2)/Ver por codigo de caja (1).cs
@@ -0,0 +1,31 @@
+﻿
+        public static void MostrarPagosPorCodigoCaja(int codigoCaja)
+        {
+            Console.WriteLine($"Mostrando pagos para el código de caja {codigoCaja}:");
+            Console.WriteLine("-------------------------------------------------------");
+
+            for (int i = 1; i < indice; i++)
+            {
+                if (numeroCaja[i] == codigoCaja)
+                {
+                    Console.WriteLine($"Número de Pago: {numeroPago[i]}");
+                    Console.WriteLine($"Fecha: {fecha[i]}");
+                    Console.WriteLine($"Hora: {hora[i]}");
+                    Console.WriteLine($"Cédula: {cedula[i]}");
+                    Console.WriteLine($"Nombre: {nombre[i]}");
+                    Console.WriteLine($"Primer Apellido: {apellido1[i]}");
+                    Console.WriteLine($"Segundo Apellido: {apellido2[i]}");
+                    Console.WriteLine($"Número de Caja: {numeroCaja[i]}");
+                    Console.WriteLine($"Tipo de Servicio: {tipoServicio[i]}");
+                    Console.WriteLine($"Número de Factura: {numeroFactura[i]}");
+                    Console.WriteLine($"Monto a Pagar: {montoPagar[i]:C}");
+                    Console.WriteLine($"Comisión: {comision[i]:C}");
+                    Console.WriteLine($"Monto Deducido: {montoDeducido[i]:C}");
+                    Console.WriteLine($"Monto Paga Cliente: {pagoCliente[i]:C}");
+                    Console.WriteLine($"Vuelto: {vuelto[i]:C}");
+                    Console.WriteLine("-------------------------------------------------------");
+                }
+            }
+        }
+
+
